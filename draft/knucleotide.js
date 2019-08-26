1892b8b07b05f75031b6259edf2495a7817e985d
anfilat <anfilat@mail.ru>
2019-08-26 12:19:14 +0300

    10%-15% for knucleotide

--- draft/knucleotide.js
+++ draft/knucleotide.js
@@ -1,141 +1,228 @@
 /* The Computer Language Benchmarks Game
    https://salsa.debian.org/benchmarksgame-team/benchmarksgame/
 
-   Contributed by Jesse Millikan
-   Modified by Matt Baker
-   Ported, modified, and parallelized by Roman Pletnev
+   Based on Node js #2 implementation of Jesse Millikan, Matt Baker and Roman Pletnev
+   Contributed by Dani Biro
 */
 
 'use strict';
 
 const rd = require('readline');
-const cluster = require('cluster');
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
 
-function RefNum(num) {
-    this.num = num;
+function RefNum (num) { this.num = num; }
+RefNum.prototype.toString = function () { return this.num.toString(); };
+
+function sliceToInt (seq, start, end) {
+	let val = 0;
+	for (let i = start; i < end; ++i) {
+		val = (val * 4) + seq[i];
+	}
+	return val;
 }
 
-RefNum.prototype.toString = function() {
-    return this.num.toString();
-};
+const keyMap = ['a', 't', 'c', 'g'];
 
-cluster.isMaster ? master() : worker();
-
-function master() {
-    const maxBuf = 2048;
-    const workers = [];
-    let reading = false;
-    let seq = '';
-
-    for (let i = 0; i < 4; ++i ) {
-        workers.push(cluster.fork({workerId: i}));
-    }
-
-    rd.createInterface(process.stdin, process.stdout)
-        .on('line', lineHandler)
-        .on('close', () => flush(true));
-
-    function lineHandler(line) {
-        if (reading) {
-            if (line[0] !== '>' && (seq += line.toUpperCase()).length >= maxBuf) {
-                flush();
-            }
-        } else {
-            reading = line.substr(0, 6) === '>THREE';
-        }
-    }
-
-    function flush(close) {
-        for (let worker of workers) {
-            worker.send(seq);
-        }
-        if (close) {
-            for (let worker of workers) {
-                worker.send('eof');
-            }
-        }
-        seq = '';
-    }
-
-    const results = [];
-    let jobs = workers.length;
-
-    function messageHandler(i) {
-        return function(message) {
-            results[i] = message;
-            if (!(--jobs)) {
-                process.stdout.write(results.join(''));
-                process.exit(0);
-            }
-        };
-    }
-
-    for (let i = 0; i < workers.length; i++) {
-        workers[i].on('message', messageHandler(i));
-    }
+function keyToString (value, length) {
+	const str = new Array(length);
+	const pow = 4;
+	for (let i = 0; i < length; ++i) {
+		const key = value - Math.floor(value / pow) * pow;
+		str[length - i - 1] = keyMap[key];
+		value = Math.floor(value / 4);
+	}
+	return str.join('').toUpperCase();
 }
 
-function worker() {
-    let seq = '';
+function frequency (seq, length) {
+	const freq = new Map();
+	const n = seq.length - length + 1;
+	const maskLength = length * 2 - 2;
+	const pow = Math.pow(2, maskLength);
+	let key = sliceToInt(seq, 0, length);
 
-    process.on('message', message => {
-        if (message === 'eof') {
-            let res = '';
-            switch (process.env.workerId) {
-                case '0':
-                    res += sort(seq, 1);
-                    res += sort(seq, 2);
-                    res += find(seq, 'GGT');
-                    break;
-                case '1':
-                    res += find(seq, 'GGTA');
-                    res += find(seq, 'GGTATT');
-                    break;
-                case '2':
-                    res += find(seq, 'GGTATTTTAATT');
-                    break;
-                case '3':
-                    res += find(seq, 'GGTATTTTAATTTATAGT');
-                    break;
-            }
-            process.send(res);
-            process.exit();
-        } else {
-            seq += message;
-        }
-    });
+	for (let i = length; i < n; ++i) {
+		const cur = freq.get(key);
+		if (cur === undefined) {
+			freq.set(key, new RefNum(1));
+		} else {
+			cur.num++;
+		}
+		key = (key - (Math.floor(key / pow) * pow)) * 4 + seq[i];
+	}
+	return freq;
 }
 
-function sort(seq, length) {
-    const f = frequency(seq, length);
-    const keys = Array.from(f.keys());
-    const n = seq.length - length + 1;
-    let res = '';
+function sort (seq, length) {
+	const f = frequency(seq, length);
+	const keys = Array.from(f.keys());
+	const n = seq.length - length + 1;
+	const res = [];
+	keys.sort((a, b) => f.get(b) - f.get(a));
+	const len = keys.length;
 
-    keys.sort((a, b) => f.get(b) - f.get(a));
-    for (let key of keys) {
-        res += `${key} ${(f.get(key) * 100 / n).toFixed(3)}\n`;
-    }
-    res += '\n';
-    return res;
+	for (let i = 0; i < len; ++i) {
+		const key = keys[i];
+		res.push(`${keyToString(key, length)} ${(f.get(key) * 100 / n).toFixed(3)}\n`);
+	}
+
+	res.push('\n');
+	return res.join('');
 }
 
-function find(seq, s) {
-    const f = frequency(seq, s.length);
-    return `${f.get(s) || 0}\t${s}\n`;
+function toTransformedBuffer (str) {
+	const sharedArray = new Uint8Array(new SharedArrayBuffer(str.length));
+	for (let i = 0; i < sharedArray.length; ++i) {
+		switch (str.charAt(i)) {
+			case 't':
+				sharedArray[i] = 1;
+				break;
+			case 'c':
+				sharedArray[i] = 2;
+				break;
+			case 'g':
+				sharedArray[i] = 3;
+				break;
+		}
+	}
+	return sharedArray;
 }
 
-function frequency(seq, length) {
-    const freq = new Map();
-    const n = seq.length - length + 1;
-    for(let i = 0; i < n ; i++) {
-        const key = seq.substr(i, length);
-        const cur = freq.get(key);
-        if (cur === undefined) {
-            freq.set(key, new RefNum(1))
-        } else {
-            ++cur.num;
-        }
-    }
-    return freq;
+function find (seq, str) {
+	const f = frequency(seq, str.length);
+	const buffer = toTransformedBuffer(str);
+	return `${f.get(sliceToInt(buffer, 0, str.length)) || 0}\t${str.toUpperCase()}\n`;
+}
+
+function master () {
+	const results = new Array(4);
+	let lines = [];
+	let reading = false;
+	let jobs = 4;
+	let currentLen = 0;
+	let processing = false;
+	let totalBack = 0;
+	const BUFFER_LIMIT = 1024 * 128;
+	let buffers = [];
+
+	const finishReading = function () {
+		const sharedArray = new Uint8Array(new SharedArrayBuffer(totalBack));
+		let index = 0;
+		for (let i = 0; i < buffers.length; ++i) {
+			sharedArray.set(buffers[i], index);
+			index += buffers[i].length;
+		}
+		processing = true;
+		buffers = [];
+		lines = [];
+		for (let i = 0; i < 4; ++i) {
+			workers[i].postMessage({ seq: sharedArray });
+		}
+	};
+
+	const messageHandler = function (workerId) {
+		return function (message) {
+			if (!processing) {
+				buffers.push(message.buf);
+				totalBack += message.buf.length;
+				if (message.done) {
+					finishReading();
+				}
+			} else {
+				results[workerId] = message;
+				if (--jobs === 0) {
+					process.stdout.write(results.join(''));
+					process.exit(0);
+				}
+			}
+		};
+	};
+
+	const workers = [...Array(jobs)].map((_, workerId) => {
+		const worker = new Worker(__filename, { workerData: { workerId } });
+		worker.on('message', messageHandler(workerId));
+		return worker;
+	});
+
+	const readOnThread = function (done) {
+		const str = lines.join('');
+		const sharedArray = new Uint8Array(new SharedArrayBuffer(str.length));
+		sharedArray.set(Buffer.from(str, 'ascii'));
+		workers[0].postMessage({ buf: sharedArray, done });
+	};
+
+	const lineHandler = function (line) {
+		if (reading) {
+			lines.push(line);
+			currentLen += line.length;
+			if (currentLen > BUFFER_LIMIT) {
+				readOnThread(false);
+				lines = [];
+				currentLen = 0;
+			}
+		} else if (line[0] === '>') {
+			reading = line.slice(0, 6) === '>THREE';
+		}
+	};
+
+	rd.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+		terminal: false
+	})
+		.on('line', lineHandler)
+		.on('close', function () {
+			readOnThread(true);
+		});
+}
+
+function transformArray (arr) {
+	for (let i = 0; i < arr.length; ++i) {
+		const x = arr[i];
+		if (x === 97) {
+			arr[i] = 0;
+		} else if (x === 116) {
+			arr[i] = 1;
+		} else if (x === 99) {
+			arr[i] = 2;
+		} else {
+			arr[i] = 3;
+		}
+	}
+}
+
+function worker () {
+	parentPort.on('message', (message) => {
+		if (message.buf) {
+			transformArray(message.buf);
+			parentPort.postMessage(message);
+		} else {
+			const seq = message.seq;
+			const res = [];
+			switch (workerData.workerId) {
+				case 0:
+					res.push(sort(seq, 1));
+					res.push(sort(seq, 2));
+					res.push(find(seq, 'ggt'));
+					break;
+				case 1:
+					res.push(find(seq, 'ggta'));
+					res.push(find(seq, 'ggtatt'));
+					break;
+				case 2:
+					res.push(find(seq, 'ggtattttaatt'));
+					break;
+				case 3:
+					res.push(find(seq, 'ggtattttaatttatagt'));
+					break;
+			}
+			parentPort.postMessage(res.join(''));
+		}
+	});
+}
+
+if (isMainThread) {
+	master();
+} else {
+	worker();
 }
