6e7810b213e5639746bf0f58327aad19a26db917
andrey filatkin <anfilat@mail.ru>
2020-02-22 15:46:49 +0300

    mpzjs for pidigits

--- draft/pidigits.js
+++ draft/pidigits.js
@@ -3,67 +3,81 @@
  *
  * contributed by Denis Gribov
  *    a translation of the C program contributed by Mr Ledhug
+ *
+ * js BigInt replaced with gmplib by Andrey Filatkin
  */
 
-const n = +process.argv[2];
+const MPZ = require('mpzjs');
 
-const chr_0 = "0".charCodeAt(0);
-const chr_t = "\t".charCodeAt(0);
-const chr_n = "\n".charCodeAt(0);
-const chr_c = ":".charCodeAt(0);
+const n = +process.argv[2] || 10000;
 
-let bufsize = (10 + 2 + n.toString().length + 1) * (n / 10);
+const char0 = '0'.charCodeAt(0);
+const charT = '\t'.charCodeAt(0);
+const charN = '\n'.charCodeAt(0);
+const charC = ':'.charCodeAt(0);
+
+let bufSize = (10 + 2 + n.toString().length + 1) * (n / 10);
 for (let i = 10, ii = 10 ** (Math.log10(n) >>> 0); i < ii; i *= 10) {
-    bufsize -= i - 1;
+    bufSize -= i - 1;
 }
 
-const buf = Buffer.allocUnsafe(bufsize);
-let bufoffs = 0;
+const buf = Buffer.allocUnsafe(bufSize);
+let bufOffs = 0;
 
 let i = 0;
 let k = 0;
-let acc = 0n;
-let den = 1n;
-let num = 1n;
+let k2 = 1;
+const acc = MPZ(0);
+const den = MPZ(1);
+const num = MPZ(1);
+const tmp = MPZ();
+const d3 = MPZ();
+const d4 = MPZ();
 
 while (i < n) {
     k++;
+    k2 += 2;
 
-    let k2 = BigInt((k << 1) + 1);
-    acc += num << 1n;
-    acc = k2 * acc;
-    den = k2 * den;
-    num = BigInt(k) * num;
+    MPZ.addMul(acc, num, 2);
+    MPZ.mul(acc, acc, k2);
+    MPZ.mul(den, den, k2);
+    MPZ.mul(num, num, k);
 
-    if (num > acc) {
+    if (num.gt(acc)) {
         continue;
     }
 
-    let tmp = 3n * num + acc;
-    const d3 = tmp / den;
+    MPZ.mul(tmp, num, 3);
+    MPZ.add(tmp, tmp, acc);
+    MPZ.div(d3, tmp, den);
 
-    tmp = tmp + num;
-    const d4 = tmp / den;
+    MPZ.add(tmp, tmp, num);
+    MPZ.div(d4, tmp, den);
 
-    if (d3 !== d4) {
+    if (d3.ne(d4)) {
         continue;
     }
 
-    buf.writeInt8(Number(d3) + chr_0, bufoffs++);
-
-    if (++i % 10 === 0) {
-        buf.writeInt8(chr_t, bufoffs++);
-        buf.writeInt8(chr_c, bufoffs++);
-
-        let str = i.toString();
-        buf.write(str, bufoffs, bufoffs += str.length);
-
-        buf.writeInt8(chr_n, bufoffs++);
+    const d = d3.toNumber();
+    buf.writeInt8(d + char0, bufOffs++);
+    i++;
+    if (i % 10 === 0) {
+        writeLineEnd(i);
     }
 
-    acc -= d3 * den;
-    acc = 10n * acc;
-    num = 10n * num;
+    MPZ.subMul(acc, den, d);
+    MPZ.mul(acc, acc, 10);
+    MPZ.mul(num, num, 10);
+}
+
+function writeLineEnd(i) {
+    buf.writeInt8(charT, bufOffs++);
+    buf.writeInt8(charC, bufOffs++);
+
+    let str = i.toString();
+    buf.write(str, bufOffs, bufOffs += str.length);
+
+    buf.writeInt8(charN, bufOffs++);
 }
 
 process.stdout.write(buf);
